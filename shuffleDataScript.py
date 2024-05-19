98281f5ab3d583a11df0a7ae21b9e7005af17dde
YAHIAG13 <78378644+YAHIAG13@users.noreply.github.com>
2024-05-19 16:55:30 +0100

    ff

--- shuffleDataScript.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-input_file = 'final_data.csv'
-output_file = 'final_data_shuffled.csv'
-
-data = pd.read_csv(input_file, header=None)
-shuffled_data = data.sample(frac=1).reset_index(drop=True)
-shuffled_data.to_csv(output_file, index=False, header=False)
